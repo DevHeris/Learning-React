ed2bce6a88eb2bb512da7fe800d4f1025170bb8c
DevHeris <badairoinioluwa578@gmail.com>
2024-04-01 18:23:18 +0100

    completed my version . Updates to be made later will be from his own way of going about it

--- challenge-1-profile-card-v1/src/index.js
+++ challenge-1-profile-card-v1/src/index.js
@@ -1,17 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="avatar.jpg" alt="devheris" />;
+}
+function Intro() {
+  return (
+    <>
+      <h1>Inioluwa Badairo</h1>
+      <p>
+        I'm a frontend developer and biochemistry student at Obafemi Awolowo
+        University. By day, I'm crafting awesome websites, and by night, I'm
+        diving into the fascinating world of biochemistry. Let's connect the
+        dots between tech and science!
+      </p>
+    </>
+  );
+}
+
+function SkillList() {
+  return (
+    <ul className="skill-list">
+      <Skill skillName="HTML &#128508;" color="red" />
+      <Skill skillName="CSS &#128509;" color="blue" />
+      <Skill skillName="Git & GitHub &#128510;" color="yellow" />
+      <Skill skillName="JavaScript &#128511;" color="green" />
+      <Skill skillName="React &#128507;" color="purple" />
+    </ul>
+  );
+}
+
+function Skill({ skillName, color }) {
+  return (
+    <li className="skill" style={{ backgroundColor: color }}>
+      {skillName}
+    </li>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
