0e7045b9fcc4d1bf5f59e41a8ab8f82cb717398a
DevHeris <badairoinioluwa578@gmail.com>
2024-04-04 20:06:27 +0100

    completed

--- challenge-2-date-counter/src/App.js
+++ challenge-2-date-counter/src/App.js
@@ -1,6 +1,72 @@
 import { useState } from "react";
 import "./App.css";
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <Counter />
+//     </div>
+//   );
+// }
+
+// function Counter() {
+//   const [step, setStep] = useState(1);
+//   const [count, setCount] = useState(0);
+
+//   const date = new Date("April 4 2024");
+//   date.setDate(date.getDate() + count);
+
+//   function nextCountHandler() {
+//     setCount((prevCount) => {
+//       return prevCount + step;
+//     });
+//   }
+
+//   function prevCountHandler() {
+//     setCount((prevCount) => prevCount - step);
+//   }
+
+//   function prevStepHandler() {
+//     setStep((prevStep) => prevStep - 1);
+//   }
+
+//   function nextStepHandler() {
+//     setStep((prevStep) => prevStep + 1);
+//   }
+
+//   return (
+//     <>
+//       <div>
+//         <button onClick={prevStepHandler}>-</button>
+//         <span>Step:{step}</span>
+//         <button onClick={nextStepHandler}>+</button>
+//       </div>
+//       <br />
+//       <div>
+//         <button onClick={prevCountHandler}>-</button>
+//         <span>Count:{count}</span>
+//         <button onClick={nextCountHandler}>+</button>
+//       </div>
+
+//       <p>
+//         {/* Nested ternary */}
+//         <span>
+//           {count === 0
+//             ? "Today is "
+//             : count > 0
+//             ? `${count} days from now is `
+//             : `${Math.abs(count)} days ago was `}
+//         </span>
+//         <span>{date.toDateString()}</span>
+//       </p>
+//     </>
+//   );
+// }
+
+// ================================== //
+// version 2
+// =============================== //
+
 function App() {
   return (
     <div className="App">
@@ -26,25 +92,31 @@ function Counter() {
     setCount((prevCount) => prevCount - step);
   }
 
-  function prevStepHandler() {
-    setStep((prevStep) => prevStep - 1);
-  }
-
-  function nextStepHandler() {
-    setStep((prevStep) => prevStep + 1);
+  function handleReset() {
+    setCount(0);
+    setStep(1);
   }
 
   return (
     <>
       <div>
-        <button onClick={prevStepHandler}>-</button>
-        <span>Step:{step}</span>
-        <button onClick={nextStepHandler}>+</button>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        {step}
       </div>
       <br />
       <div>
         <button onClick={prevCountHandler}>-</button>
-        <span>Count:{count}</span>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
         <button onClick={nextCountHandler}>+</button>
       </div>
 
@@ -59,6 +131,11 @@ function Counter() {
         </span>
         <span>{date.toDateString()}</span>
       </p>
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : null}
     </>
   );
 }
--- exercise-1-flash-cards/src/App.js
+++ exercise-1-flash-cards/src/App.js
@@ -47,7 +47,7 @@ function FlashCards() {
   const [selectedId, setSelectedId] = useState(null);
 
   function handleSelected(id) {
-    setSelectedId(id);
+    setSelectedId(id !== selectedId ? id : null);
   }
 
   return (
