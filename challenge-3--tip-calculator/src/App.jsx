416703a24feae806af1de582a946a670ba2d4b0e
DevHeris <badairoinioluwa578@gmail.com>
2024-04-06 20:29:47 +0100

    still having issues

---
+++ challenge-3--tip-calculator/src/App.jsx
@@ -0,0 +1,73 @@
+/* eslint-disable react/prop-types */
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [bill, setBill] = useState("");
+  const [tip, setTip] = useState(0);
+
+  return (
+    <>
+      <BillInput bill={bill} onInput={setBill} />
+      <br />
+      <SelectPercentage tip={tip} onSelect={setTip} />
+      {bill && <p>You pay {`$${bill + tip} ($${bill} + $${tip} tip)`}</p>}
+    </>
+  );
+}
+
+function BillInput({ bill, onInput }) {
+  return (
+    <>
+      <label>How much was the bill ? </label>
+      <input
+        type="text"
+        value={bill}
+        onChange={(e) => {
+          onInput(Number(e.target.value));
+        }}
+      />
+    </>
+  );
+}
+
+function SelectPercentage({ tip, onSelect }) {
+  const options = [
+    { description: "Dissatisfied", percent: 0 },
+    { description: "It was okay", percent: 5 },
+    { description: "It was good", percent: 10 },
+    { description: "Absolutely amazing!", percent: 20 },
+  ];
+
+  return (
+    <>
+      <SelectInput tip={tip} onSelect={onSelect} options={options} who="tipper">
+        How did you like the service ?
+      </SelectInput>
+      <br />
+      <SelectInput options={options} tip={tip} onSelect={onSelect} who="friend">
+        How did your friend like the service ?
+      </SelectInput>
+    </>
+  );
+}
+
+function SelectInput({ options, children, tip, onSelect, who }) {
+  function setTipHandler(e) {
+    onSelect((curTip) => curTip + Number(e.target.value));
+  }
+  return (
+    <>
+      <label>{children}</label>
+      <select value={20} onChange={setTipHandler}>
+        {options.map((option) => (
+          <option value={option.percent} key={option.percent}>
+            {option.description} ({option.percent}%)
+          </option>
+        ))}
+      </select>
+    </>
+  );
+}
+
+export default App;
