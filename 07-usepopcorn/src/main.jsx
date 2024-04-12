897abd3390adaad188dbb3590220e30e9b3ab5ce
DevHeris <badairoinioluwa578@gmail.com>
2024-04-12 15:33:46 +0100

    allows the  user to configure the star rating component as they like

--- 07-usepopcorn/src/main.jsx
+++ 07-usepopcorn/src/main.jsx
@@ -4,10 +4,31 @@ import ReactDOM from "react-dom/client";
 // import './index.css'
 
 import StarRating from "./StarRating.jsx";
+import { useState } from "react";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <>
+      <StarRating onSetRating={setMovieRating} />
+      <p>This movie has a rating of {movieRating} stars</p>
+    </>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <StarRating maxRating={10} />
+    <Test />
+    <StarRating />
+    <StarRating
+      maxRating={5}
+      color="red"
+      size="48"
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={2}
+    />
+    <StarRating maxRating={10} color="purple" size="30" defaultRating={9} />
     {/* <App /> */}
   </React.StrictMode>
 );
