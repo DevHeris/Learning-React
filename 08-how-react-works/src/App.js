30ae72bf68bcaefee85bd581f1cc3ac7e62881c5
DevHeris <badairoinioluwa578@gmail.com>
2024-04-15 17:17:16 +0100

    State batching

--- 08-how-react-works/src/App.js
+++ 08-how-react-works/src/App.js
@@ -66,9 +66,25 @@ function TabContent({ item }) {
   const [showDetails, setShowDetails] = useState(true);
   const [likes, setLikes] = useState(0);
 
-  function handleInc() {
-    setLikes(likes + 1);
-  }
+  const handleInc = () => {
+    setLikes((likes) => likes + 1);
+  };
+
+  const handleTrippleInc = () => {
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1);
+  };
+
+  const handleUndo = () => {
+    // Batched state update
+    setShowDetails(true);
+    setLikes(0);
+  };
+
+  const handleUndoLater = () => {
+    setTimeout(handleUndo, 2000);
+  };
 
   return (
     <div className="tab-content">
@@ -83,13 +99,13 @@ function TabContent({ item }) {
         <div className="hearts-counter">
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={handleTrippleInc}>+++</button>
         </div>
       </div>
 
       <div className="tab-undo">
-        <button>Undo</button>
-        <button>Undo in 2s</button>
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
       </div>
     </div>
   );
