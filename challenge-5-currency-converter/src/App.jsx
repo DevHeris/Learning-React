10da19391f94bf41c4c1c85382b05848ef118707
DevHeris <badairoinioluwa578@gmail.com>
2024-04-18 16:31:00 +0100

    final

--- challenge-5-currency-converter/src/App.jsx
+++ challenge-5-currency-converter/src/App.jsx
@@ -1,38 +1,28 @@
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function App() {
-  const [amount, setAmount] = useState("");
-  const [from, setFrom] = useState("USD");
-  const [to, setTo] = useState("EUR");
-  const [output, setOutput] = useState(null);
+  const [amount, setAmount] = useState(1);
+  const [fromCur, setFromCur] = useState("EUR");
+  const [toCur, setToCur] = useState("USD");
+  const [converted, setConverted] = useState("");
 
-  useEffect(() => {
-    const controller = new AbortController();
+  useEffect(
+    function () {
+      async function convert() {
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCur}&to=${toCur}`
+        );
+        const data = await res.json();
+        setConverted(data.rates[toCur]);
+      }
 
-    async function fetchResult() {
-      const resp = await fetch(
-        `https://api.frankfurter.app/latest?amount=${amount}&from=${from}&to=${to}`,
-        { signal: controller.signal }
-      );
-
-      const { rates } = await resp.json();
-
-      if (!rates) return;
-
-      const rate = rates[to];
-
-      setOutput(rate);
-    }
-
-    fetchResult();
-
-    return () => {
-      controller.abort();
-    };
-  }, [amount, from, to]);
+      if (fromCur === toCur) return setConverted(amount);
+      convert();
+    },
+    [amount, fromCur, toCur]
+  );
 
   return (
     <div>
@@ -41,24 +31,21 @@ export default function App() {
         value={amount}
         onChange={(e) => setAmount(Number(e.target.value))}
       />
-      <select value={from} onChange={(e) => setFrom(e.target.value)}>
+      <select value={fromCur} onChange={(e) => setFromCur(e.target.value)}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select value={to} onChange={(e) => setTo(e.target.value)}>
+      <select value={toCur} onChange={(e) => setToCur(e.target.value)}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-
-      {output && (
-        <p>
-          {output} {to}
-        </p>
-      )}
+      <p>
+        {converted} {toCur}
+      </p>
     </div>
   );
 }
