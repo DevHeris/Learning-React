0c49a9e8782c197b74aa8c89f232f93ffc5b114c
DevHeris <badairoinioluwa578@gmail.com>
2024-04-25 09:12:36 +0100

    useGeolocate custom hook challenge

---
+++ challenge-6-useGeolocate/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { useGeolocate } from "./useGeolocate";
+
+export default function App() {
+  const [countClicks, setCountClicks] = useState(0);
+
+  const { isLoading, getPosition, error, lng, lat } = useGeolocate();
+
+  function handleClick() {
+    setCountClicks((count) => count + 1);
+    getPosition();
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick} disabled={isLoading}>
+        Get my position
+      </button>
+
+      {isLoading && <p>Loading position...</p>}
+      {error && <p>{error}</p>}
+      {!isLoading && !error && lat && lng && (
+        <p>
+          Your GPS position:{" "}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href={`https://www.openstreetmap.org/#map=16/${lat}/${lng}`}
+          >
+            {lat}, {lng}
+          </a>
+        </p>
+      )}
+
+      <p>You requested position {countClicks} times</p>
+    </div>
+  );
+}
