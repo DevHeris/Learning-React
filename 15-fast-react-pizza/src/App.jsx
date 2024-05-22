140f3f3f290a6195f3cd3e7f7b3ca4c834417c71
DevHeris <badairoinioluwa578@gmail.com>
2024-05-22 08:33:02 +0100

    update : setup loader whenever data is still being loaded

--- 15-fast-react-pizza/src/App.jsx
+++ 15-fast-react-pizza/src/App.jsx
@@ -1,11 +1,23 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import Cart from "./features/cart/Cart";
+import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/menu", element: <Menu /> },
+  {
+    element: <AppLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/menu", element: <Menu />, loader: menuLoader },
+      { path: "/order/new", element: <CreateOrder /> },
+      { path: "/order/:orderID", element: <Order /> },
+      { path: "/cart", element: <Cart /> },
+    ],
+  },
 ]);
 
 function App() {
