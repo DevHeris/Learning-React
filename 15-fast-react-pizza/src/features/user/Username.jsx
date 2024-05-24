2f7fd14839102acdf247316bbc9d7dc393e73db8
DevHeris <badairoinioluwa578@gmail.com>
2024-05-24 07:40:41 +0100

    update

--- 15-fast-react-pizza/src/features/user/Username.jsx
+++ 15-fast-react-pizza/src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Heris</div>;
+  return <div className="hidden text-sm font-semibold sm:block">Heris</div>;
 }
 
 export default Username;
--- 15-fast-react-pizza/src/ui/Home.jsx
+++ 15-fast-react-pizza/src/ui/Home.jsx
@@ -3,7 +3,7 @@ import CreateUser from "../features/user/CreateUser";
 function Home() {
   return (
     <div className="my-10 text-center">
-      <h1 className="mb-8  text-xl font-semibold ">
+      <h1 className="mb-8 px-4 text-xl font-semibold md:text-3xl">
         The best pizza.
         <br />
         <span className="text-yellow-500">
