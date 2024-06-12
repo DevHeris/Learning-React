572c978e60a764c3a4263d77dc36d54c1e9acb78
DevHeris <badairoinioluwa578@gmail.com>
2024-06-12 19:27:33 +0100

    storage bucket setup

--- 16-the-wild-oasis/src/pages/Cabins.jsx
+++ 16-the-wild-oasis/src/pages/Cabins.jsx
@@ -15,7 +15,12 @@ function Cabins() {
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
-      <p></p>
+      <p>
+        <img
+          src="https://zhgcozyfjmjzkbvvsufb.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg"
+          alt=""
+        />
+      </p>
     </Row>
   );
 }
