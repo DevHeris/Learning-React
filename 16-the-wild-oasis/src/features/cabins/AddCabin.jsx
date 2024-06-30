8eca143600165ff65b6d9f192ceb13ceaf4484b5
DevHeris <badairoinioluwa578@gmail.com>
2024-06-30 03:38:23 +0100

    using the modal window for editing form and deleting confirmation

--- 16-the-wild-oasis/src/features/cabins/AddCabin.jsx
+++ 16-the-wild-oasis/src/features/cabins/AddCabin.jsx
@@ -1,7 +1,6 @@
 import Modal from "../../ui/Modal";
 import Button from "../../ui/Button";
 import CreateCabinForm from "./CreateCabinForm";
-import CabinTable from "./CabinTable";
 
 function AddCabin() {
   return (
@@ -11,12 +10,6 @@ function AddCabin() {
       </Modal.Open>
       <Modal.Window name="cabin-form">
         <CreateCabinForm />
-      </Modal.Window>
-      <Modal.Open opens="table">
-        <Button>Show table</Button>
-      </Modal.Open>
-      <Modal.Window name="table">
-        <CabinTable />
       </Modal.Window>
     </Modal>
   );
